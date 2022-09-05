c9f7de536a8ef6b1837d0f021ca8dc95e950c0f6
Artem <artem.kh4rchenko@ya.ru>
2022-09-05 11:05:29 +0700

    Initial Commit

---
+++ MyCalctosh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalctosh
+//
+//  Created by Артём Харченко on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
